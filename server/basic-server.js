727b389b657c5e84cdb55609510afa556bdec05e
Fred Zirdung <fred@hackreactor.com>
2014-01-26 20:46:14 -0800

    Initial commit

---
+++ server/basic-server.js
@@ -0,0 +1,32 @@
+/* Import node's http module: */
+var http = require("http");
+
+/* Every server needs to listen on a port with a unique number. The
+ * standard port for HTTP servers is port 80, but that port is
+ * normally already claimed by another server and/or not accessible
+ * so we'll use a higher port number that is not likely to be taken: */
+var port = 8080;
+
+/* For now, since you're running this server on your local machine,
+ * we'll have it listen on the IP address 127.0.0.1, which is a
+ * special address that always refers to localhost. */
+var ip = "127.0.0.1";
+
+/* We use node's http module to create a server. Note, we called it 'server', but
+we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
+will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
+Lastly, we tell the server we made to listen on the given port and IP. */
+var server = http.createServer(handleRequest);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
+
+/* To start this server, run:
+     node basic-server.js
+ *  on the command line.
+
+ * To connect to the server, load http://127.0.0.1:8080 in your web
+ * browser.
+
+ * server.listen() will continue running as long as there is the
+ * possibility of serving more requests. To stop your server, hit
+ * Ctrl-C on the command line. */
---
+++ server/request-handler.js
@@ -0,0 +1,45 @@
+/* You should implement your request handler function in this file.
+ * And hey! This is already getting passed to http.createServer()
+ * in basic-server.js. But it won't work as is.
+ * You'll have to figure out a way to export this function from
+ * this file and include it in basic-server.js so that it actually works.
+ * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
+
+var handleRequest = function(request, response) {
+  /* the 'request' argument comes from nodes http module. It includes info about the
+  request - such as what URL the browser is requesting. */
+
+  /* Documentation for both request and response can be found at
+   * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
+
+  console.log("Serving request type " + request.method + " for url " + request.url);
+
+  var statusCode = 200;
+
+  /* Without this line, this server wouldn't work. See the note
+   * below about CORS. */
+  var headers = defaultCorsHeaders;
+
+  headers['Content-Type'] = "text/plain";
+
+  /* .writeHead() tells our server what HTTP status code to send back */
+  response.writeHead(statusCode, headers);
+
+  /* Make sure to always call response.end() - Node will not send
+   * anything back to the client until you do. The string you pass to
+   * response.end() will be the body of the response - i.e. what shows
+   * up in the browser.*/
+  response.end("Hello, World!");
+};
+
+/* These headers will allow Cross-Origin Resource Sharing (CORS).
+ * This CRUCIAL code allows this server to talk to websites that
+ * are on different domains. (Your chat client is running from a url
+ * like file://your/chat/client/index.html, which is considered a
+ * different domain.) */
+var defaultCorsHeaders = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10 // Seconds.
+};
